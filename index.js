1412a2d4cf794a7834882020097e29948e35cad2
antobrines <antonin.brines@outlook.fr>
2024-02-26 12:45:29 +0100

    feat: 🎸 add count

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const cron = require('node-cron');
 const execSync = require('child_process').exec;
 
+var count = 1;
 cron.schedule('*/5 * * * * *', () => {
-    execSync('powershell -File ./test.ps1', (err, stdout, stderr) => {
+    execSync(`powershell -File ./test.ps1 "${count++}"`, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return;
         }
-        console.log(stdout);
+        console.log(stdout.toString());
     }
     );
 });
--- test.ps1
+++ test.ps1
@@ -1,5 +1,7 @@
 $bddUser = 'admin'
+$bddPassword = 'admin'
+$count = $args[0]
 
-mysqldump -u $bddUser -padmin music > music.sql
+mysqldump -u $bddUser --password=$bddPassword music > music.sql
 
-Write-Host "Backup done"
+Write-Host "Backup number $count is done !" 
